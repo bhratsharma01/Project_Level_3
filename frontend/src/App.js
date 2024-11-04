518219b2b572b3f4f82f22ee718d212cb0429ab3
bhratsharma01 <bhratsharma619@gmail.com>
2024-11-04 15:06:25 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import CreateProject from './components/CreateProject';
+import ProjectList from './components/ProjectList';
 
 function App() {
-  const [projects, setProjects] = useState([]);
+    const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_BASE_URL}/api/projects`)
-      .then(response => response.json())
-      .then(data => setProjects(data))
-      .catch(error => console.error('Error fetching projects:', error));
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>Projects</h1>
-      <ul>
-        {projects.map(project => (
-          <li key={project._id}>
-            {project.title}: {project.description}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Home apiBaseUrl={apiBaseUrl} />} />
+                    <Route path="/create-project" element={<CreateProject apiBaseUrl={apiBaseUrl} />} />
+                    <Route path="/projects" element={<ProjectList apiBaseUrl={apiBaseUrl} />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
+
