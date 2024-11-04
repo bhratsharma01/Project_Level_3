568e48b938e6f354ef6f292261809bebb5aa7e70
bhratsharma01 <bhratsharma619@gmail.com>
2024-11-04 10:31:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
